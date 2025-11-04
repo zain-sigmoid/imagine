78030d6df5fc6d477408399dcaf883a43b618654
Mohd Zain <zainmohd@sigmoidanalytics.com>
2025-11-04 13:04:42 +0530

    updated function

--- core/utils.py
+++ core/utils.py
@@ -4,9 +4,12 @@ import yaml
 
 class Utility:
     @staticmethod
-    def load_template(filepath="templates.yml"):
+    def load_template(filepath="templates.yml", template="prompt"):
         base_path = os.path.dirname(__file__)  # folder where utils.py is
         full_path = os.path.join(base_path, filepath)
         with open(full_path, "r") as f:
             data = yaml.safe_load(f)
-        return data["NAPKIN_TEMPLATE"]
+        if template == "napkin":
+            return data["NAPKIN_TEMPLATE"]
+        else:
+            return data["PROMPT_TEMPLATE"]
